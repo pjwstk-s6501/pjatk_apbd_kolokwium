60f5cf1657c08bee5fd56b6a0a315c0a82385867
Piotr Chmielewski <264848712+pjwstk-s6501@users.noreply.github.com>
2026-06-07 20:15:02 +0200

    Empty api controller

---
+++ pjatk_apbd_kolokwium/Controllers/NurseriesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace pjatk_apbd_kolokwium.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NurseriesController : ControllerBase
+{
+    
+}
